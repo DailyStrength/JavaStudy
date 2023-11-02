9284debe1cbac7bbc124f7978306095f014516e3
조형근 <jhkluck22@naver.com>
2023-11-02 19:31:50 +0900

    Update Main.java

--- Standard/src/Main.java
+++ Standard/src/Main.java
@@ -1,5 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         /*System.out.println("Hello, world");
@@ -12,5 +13,38 @@ public class Main {
 
         x = 10;
         System.out.println(x);*/
+
+       // System.out.print("Hello World");
+//        int x= 10, y=5;
+//        System.out.println("x="+x);
+//        System.out.println("y="+y);
+//
+//        int tmp = x;
+//        x=y;
+//        y=tmp;
+//        System.out.println("x="+x);
+//        System.out.println("y="+y);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("두자리 정수를 하나 입력해주세요>");
+//        String input = scanner.nextLine();
+//        int num = Integer.parseInt(input);
+//
+//        System.out.println("입력내용 :"+input);
+//        System.out.printf("num=%d%n", num);
+
+//        String str= "3";
+//
+//        System.out.println(str.charAt(0)-'0');
+//        System.out.println('3' - '0' +1);
+//        System.out.println(Integer.parseInt("3")+1);
+//        System.out.println("3" + 1);
+//        System.out.println((char)(3+'0'));
+
+
+        double d = 85.4;
+        int score = (int) d;
+        System.out.println("score="+score);
+        System.out.println("d="+d);
     }
 }
